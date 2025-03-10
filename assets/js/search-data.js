c3a1933ef1a8726c5e9b5f8a954844fb93c7a39a
hao-vuong <haowangbright@gmail.com>
2025-03-10 05:37:19 +0000

    Deploying to gh-pages from @ hao-vuong/hao-vuong.github.io@38f26dd6460f92702f75fd2caed72957d45e3616 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -379,9 +379,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+          section: "News",},{id: "projects-dummy",
+          title: 'dummy',
+          description: "to be updated",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{
